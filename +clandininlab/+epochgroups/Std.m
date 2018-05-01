1725b34fdd295f11b8b783fba8670f148b326e57
Max Turner <maxwellholteturner@gmail.com>
2018-05-01 14:16:32 -0700

    New std epochGroup

---
+++ +clandininlab/+epochgroups/Std.m
@@ -0,0 +1,14 @@
+classdef Std < clandininlab.epochgroups.EpochGroup
+    
+    methods
+        
+        function obj = Std()
+            import symphonyui.core.*;
+            
+            obj.addAllowableParentType([]);
+        end
+        
+    end
+    
+end
+
