72bd174f9679c14bffddaabeab49c0739084e4a1
Max Turner <maxwellholteturner@gmail.com>
2018-08-18 10:53:48 -0700

    New Lmrf mapping, changes to moving bar
    -Moving bar trajectory now centered on center, so should hit center at same time in trajectory each run
    -New local motion rf mapping protocol
    Need to test both on rig...

--- +turner/+protocols/LmrfMapping.m
+++ +turner/+protocols/LmrfMapping.m
@@ -1,24 +1,28 @@
-classdef MovingBar < clandininlab.protocols.ClandininLabStageProtocol
+classdef LmrfMapping < clandininlab.protocols.ClandininLabStageProtocol
+    
     properties
-        preTime = 500                  % Leading duration (ms)
-        stimTime = 3000                 % Duration (ms)
-        tailTime = 500                 % Trailing duration (ms)
-        intensity = 0                 % Bar intensity (0-1)
-        barWidth = 20                   % Deg. visual angle, parallel to axis of movement
-        barHeight = 40                  % Deg. visual angle, Perp. to axis of movement
-        barSpeed = 60                   % Deg./sec.
-        orientations = [0 45 90 135 180 225 270 315]  % Deg.
-        center = [0, 30]                % Deg. (az., el.)
+        preTime = 500                   % Leading duration (ms)
+        stimTime = 2000                 % Duration (ms)
+        tailTime = 500                  % Trailing duration (ms)
+        intensity = 0                   % intensity of square (0-1)
         backgroundIntensity = 0.5       % Background light intensity (0-1)
-        numberOfAverages = uint16(40)    % Number of epochs
-        randomizeOrder = true           % Randomize sequence of orientations t/f
+        
+        speed = 60                      % Deg/sec.
+        squareSize = 10                 % Deg.
+        azimuths = -30:10:30            % Deg.
+        elevations = 5:10:35            % Deg.   
+        orientations = [0 90 180 270]   % Deg.
+        numberOfAverages = uint16(100)  % Number of epochs
+        randomizeOrder = true          % Randomize sequence of locations t/f
     end
     
     properties (Hidden)
-        orientationSequence
+        AzElOr_sequence
+        currentAzimuth
+        currentElevation
         currentOrientation
     end
-    
+
     methods
         
         function didSetRig(obj)
@@ -37,22 +41,27 @@ classdef MovingBar < clandininlab.protocols.ClandininLabStageProtocol
         function prepareRun(obj)
             prepareRun@clandininlab.protocols.ClandininLabStageProtocol(obj);
             
-            % Create sequences for orientation
-            obj.orientationSequence = obj.orientations;
+            % Create Azimuth, Elevation, Orientation sequence
+            [Az, El, Or] = meshgrid(obj.azimuths, obj.elevations, obj.orientations);
+            obj.AzElOr_sequence = [Az(:), El(:), Or(:)];
         end
         
         function prepareEpoch(obj, epoch)
             prepareEpoch@clandininlab.protocols.ClandininLabStageProtocol(obj, epoch);
             
-            % Determine current orientation
-            index = mod(obj.numEpochsCompleted, length(obj.orientationSequence)) + 1;
-            % Randomize the bar orientation order at the beginning of each sequence.
+            % Determine current azimuth & elevation
+            index = mod(obj.numEpochsCompleted, size(obj.AzElOr_sequence,1)) + 1;
+            % Randomize the sequence order at the beginning of each sequence.
             if index == 1 && obj.randomizeOrder
-                randInds = randperm(length(obj.orientationSequence));
-                obj.orientationSequence = obj.orientationSequence(randInds);
+                randInds = randperm(size(obj.AzElOr_sequence,1));
+                obj.AzElOr_sequence = obj.AzElOr_sequence(randInds,:);
             end
-            obj.currentOrientation = obj.orientationSequence(index);
+            obj.currentAzimuth = obj.AzElOr_sequence(index,1);
+            obj.currentElevation = obj.AzElOr_sequence(index,2);
+            obj.currentOrientation = obj.AzElOr_sequence(index,3);
             
+            epoch.addParameter('currentAzimuth', obj.currentAzimuth);
+            epoch.addParameter('currentElevation', obj.currentElevation);
             epoch.addParameter('currentOrientation', obj.currentOrientation);
         end
         
@@ -61,23 +70,22 @@ classdef MovingBar < clandininlab.protocols.ClandininLabStageProtocol
             
             % Set background intensity for presentation
             p.setBackgroundColor(obj.backgroundIntensity);
-            
+
             % "Rectangle" stimulus:
             Rect = clandininlab.stimuli.Rectangle();
-            Rect.width = obj.barWidth;
-            Rect.height = obj.barHeight;
+            Rect.width = obj.squareSize;
+            Rect.height = obj.squareSize;
             Rect.color = obj.intensity;
             Rect.rectOrientation = obj.currentOrientation;
-            
-            
+
             distanceTraveled = obj.barSpeed * obj.stimTime; %deg
             
-            thetaController = stage.builtin.controllers.PropertyController(Rect, 'azimuth',@(state)obj.center(1) +...
+            thetaController = stage.builtin.controllers.PropertyController(Rect, 'azimuth',@(state)obj.currentAzimuth +...
                 cosd(obj.currentOrientation)*(-distanceTraveled/2 + obj.barSpeed*state.time));
             
-            phiController = stage.builtin.controllers.PropertyController(Rect, 'elevation',@(state)obj.center(2) +...
+            phiController = stage.builtin.controllers.PropertyController(Rect, 'elevation',@(state)obj.currentElevation +...
                 sind(obj.currentOrientation)*(-distanceTraveled/2 + obj.barSpeed*state.time));
-            
+
             p.addStimulus(Rect);
             p.addController(thetaController);
             p.addController(phiController);
@@ -86,7 +94,7 @@ classdef MovingBar < clandininlab.protocols.ClandininLabStageProtocol
             rectVisible = stage.builtin.controllers.PropertyController(Rect, 'visible', @(state)state.time >= obj.preTime * 1e-3 && state.time < (obj.preTime + obj.stimTime) * 1e-3);
             p.addController(rectVisible);
         end
-
+  
         function tf = shouldContinuePreparingEpochs(obj)
             tf = obj.numEpochsPrepared < obj.numberOfAverages;
         end
